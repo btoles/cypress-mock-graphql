6934152208f4ab77c37d351e7e1177b5c4196f98
Brennan Toles <btoles@users.noreply.github.com>
2019-04-25 11:16:44 -0500

    chore: Remove warning and add conditional
    Adding logic around the execution of our mock response -- do not stub if we've already responded as many times as we had mocks.

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 Cypress.Commands.add('mockGraphQL', { prevSubject: false }, (...mocks) => {
-    let requestsMocked = 0;
-    Cypress.on('window:before:load', win => {
+	let requestsMocked = 0;
+	Cypress.on('window:before:load', win => {
         /** 
          * If we've already wrapped 'fetch' we need to restore the stub to allow for overriding
          * with another call to mockGraphQL.
          */
-        if (win.fetch && win.fetch.restore) {
-            win.fetch.restore();
-        }
-        cy.stub(win, 'fetch', async (...args) => {
-            const [requestUri] = args;
-            if (requestUri.includes('/graphql')) {
-                /** Alert the user that there are more GraphQL calls than there are mocks. */
-                if (requestsMocked >= mocks.length) {
-                    Cypress.log({
-                        message: 'WARNING: The number of GraphQL requests exceeded the number of mocks provided.'
-                    });
-                }
-                const body = new Blob([JSON.stringify(mocks[requestsMocked++], null, 2)], {type : 'application/json'})
-                return new Response(body)
-            }
-        })
-    });
+		if (win.fetch && win.fetch.restore) {
+			win.fetch.restore();
+		}
+		if (requestsMocked < mocks.length) {
+			cy.stub(win, 'fetch', async (...args) => {
+				const [requestUri] = args;
+				if (requestUri.includes('/graphql')) {
+					const body = new Blob([JSON.stringify(mocks[requestsMocked++], null, 2)], { type: 'application/json' })
+					return new Response(body)
+				}
+			});
+		}
+	});
 });
